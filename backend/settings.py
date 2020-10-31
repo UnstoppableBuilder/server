eeb688ba61938775e965e00900ad6284ef22dcf5
Pavel Kozlov <mail@pkozlov.ru>
2020-10-31 14:52:22 +0300

    settings

--- backend/settings.py
+++ backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'm%i$oy96l*mb$u7--nn)jnkjm_l3f-z5l(zu)g5yu6j6cfxk3a'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ub.pkozlov.ru', '127.0.0.1']
 
 
 # Application definition
@@ -141,7 +141,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/s/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
